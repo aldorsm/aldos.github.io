029d06f467ff998b868b8a975a286fe3d1179724
Aldo Santizo <aldorsm@gmail.com>
2019-02-19 15:31:49 -0800

    Aldo Santizo

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Local Geek</title>
+	<link type="text/css" rel="stylesheet" href="style/core.css" />
+</head>
+<body>
+	<header>
+		<h1>Your Local Geek</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">About</li>
+				<li><a href="index.php?Services">Services</li>
+				<li><a href="index.php?Help Center">Help Center</li>
+				<li><a href="index.php?Contact">Contact</li>
+			</ul>
+		</nav>
+	</header>
+<a href="myLink"><span class="otherPage"></span></a>
+
+	<div id="content">
+			<h3>Content</h3>
+
+
+	</div>
+
+	<footer>
+
+	<p>© <?php echo date("Y"); ?> Aldo Santizo All Rights Reserved</p>
+	</footer>
+
+</body>
+</html>
